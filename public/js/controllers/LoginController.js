87b9d1fe5cc16ba8b8da858f99c44277e2c6f844
Giovanni D'Auria <giovanni.dauria@accenture.com>
2018-07-16 15:20:27 +0200

    Login reale

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -15,48 +15,70 @@ angular.module("gestionaleApp")
 
 	//funzione che effettua la login
 	$scope.login = function (){
-
-		$( ".page-content").css( "margin-left", "235px" );
-		$( ".page-container").css("margin-top", "75px").css("padding", "20px 20px 0");
-		$("#menuTop").show();
-		$("#menuSide").show();
-		$("#navLatr").show();
-		$sessionStorage.userMenu = {}; //angular.fromJson(response.data.MENU);
-		$sessionStorage.user = {}//response.data;
-		$scope.user = $sessionStorage.user;	
-		$scope.userMenu = $sessionStorage.userMenu;
-		$location.url('/home');
-
-		//TODO effettuare login
-
 		//invocazione service
-		/*
+	
 		LoginService.checkLogin($scope.username, $scope.password).then(function(response) { 
 	    	if(response!== null && response.data !== null && response.data !=='' ){
 	    		if (response.data.FORCE_CHANGE_PWD === 1) {
+					$rootScope.userReset = response.data.USERNAME;
 					$location.url('/reset');
 				} else {
 
-					
+					/*AziendaService.getDatiAzienda().then(function(response) {  
+						var handleResponseResult = $scope.handleResponse(response);  
+						if(handleResponseResult.next){
+							$sessionStorage.azienda = response.data.azienda;
+							if(response.data.azienda.LOGO_CONTENT_TYPE){
+								$sessionStorage.azienda.urlImage = "data:"+response.data.azienda.LOGO_CONTENT_TYPE+";base64,"+response.data.azienda.LOGO;
+							}else{
+								$sessionStorage.azienda.urlImage = "";
+							}
+							$scope.datiazienda = $sessionStorage.azienda;	
+						} else {
+							toastr.error("Errore: "+ handleResponseResult.errorCode + " - GESTIONE ERRORE DA FARE!!!" );
+						}
+					});	*/
 
-				}
+					//Fare check Azienda
+
+					// AziendaService.esisteAzienda().then(function(responseAz) { 
+						//if(responseAz!== null && responseAz.data !== null && responseAz.data !=='' ){
+							$( ".page-content").css( "margin-left", "235px" );
+							$( ".page-container").css("margin-top", "75px").css("padding", "20px 20px 0");
+							$("#menuTop").show();
+							$("#menuSide").show();
+							$("#navLatr").show();
+							$sessionStorage.userMenu = angular.fromJson(response.data.MENU);
+							$sessionStorage.user = response.data;
+							$scope.user = $sessionStorage.user;	
+							$scope.userMenu = $sessionStorage.userMenu;
+							$location.url('/home');
+						/*}else{
+							$scope.isInError=true;
+							console.log('Si è verificato un errore, si prega di contattare l\'amministratore');
+							$scope.erroreLogin = 'Si è verificato un errore, si prega di contattare l\'amministratore';*/
+						}
+					// });
+
+				//}
 
 	    	}else{
 	    		$scope.isInError=true;
 				console.log('Errore login - credenziali errate ');
 				$scope.erroreLogin = 'Errore login - credenziali errate';
 	    	}
-		});	
-		*/
+	    });	
 	}
 
 	
     
     $scope.resetPwd = function (){
 		//invocazione service
-		LoginService.resetPassword($scope.username, $scope.oldpassword, $scope.newpassword).then(function(response) {  
+		LoginService.resetPassword($rootScope.userReset, $scope.oldpassword, $scope.newpassword).then(function(response) {  
 	    	if(response!== null && response.data !== null && response.data !=='' ){
-	    		$scope.password = $scope.newpassword;
+				$scope.username = $rootScope.userReset;
+				$scope.password = $scope.newpassword;
+				delete $rootScope.userReset;
 	    		toastr.success("password cambiata");
 	    		$scope.login();
 	    	}else{
