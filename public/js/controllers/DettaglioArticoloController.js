6992b16c70e3dcd52ce3ec906f64b4c17683ab49
cfontanarosa <clafont79@hotmail.com>
2018-07-18 18:43:55 +0200

    gestione scheda articolo con bozza di grafico

--- public/js/controllers/DettaglioArticoloController.js
+++ public/js/controllers/DettaglioArticoloController.js
@@ -1,9 +1,10 @@
 angular.module("gestionaleApp")
-.controller("ArticoliController",
- ['$scope','$uibModal','filterFilter','$sessionStorage','ArticoliService','CategorieService','CommonService', 'DTOptionsBuilder', 'DTColumnDefBuilder',
- function ($scope, $uibModal, filterFilter, $sessionStorage, ArticoliService, CategorieService, CommonService, DTOptionsBuilder, DTColumnDefBuilder) {
+.controller("DettaglioArticoloController",
+ ['$scope','$uibModal','filterFilter', '$routeParams','$sessionStorage','ArticoliService','CategorieService','CommonService', 'DTOptionsBuilder', 'DTColumnDefBuilder',
+ function ($scope, $uibModal, filterFilter, $routeParams, $sessionStorage, ArticoliService, CategorieService, CommonService, DTOptionsBuilder, DTColumnDefBuilder) {
 	 'use strict';
-	$scope.persistent.idArticolo = $routeParams.idCliente;
+	$scope.persistent = {};
+	$scope.persistent.idArticolo = $routeParams.idArticolo;
 
 	$scope.dtOptions = DTOptionsBuilder.newOptions().withOption('responsive', true).withLanguageSource('//cdn.datatables.net/plug-ins/1.10.16/i18n/Italian.json');
 	$scope.dtColumnDefs = [          
@@ -20,24 +21,99 @@ angular.module("gestionaleApp")
 	$scope.filters.filter = {};
 	// START PUBLIC FUNCTIONS
 
-	$scope.schedaArticolo = function(idArticolo){
-		ArticoliService.getArticoloById(idArticolo).then(function(response) {  
+	$scope.schedaArticolo = function(){
+		ArticoliService.getArticoloById($scope.persistent.idArticolo).then(function(response) {  
 			var handleResponseResult = $scope.handleResponse(response);  
 				if(handleResponseResult.next){
-					$scope.persistent.articolo = response.data.articolo[0];	
+					$scope.persistent.articolo = response.data.articolo;	
 				} else {
 					toastr.error("TODO - GESTIONE ERRORE");
 				}		    	
 		});	
 	}
 
+
+	$scope.loadChart = function () {
+		
+		var chart = new CanvasJS.Chart("chartArticolo", {
+			animationEnabled: true,
+			title:{
+				text: "Crude Oil Reserves vs Production, 2016"
+			},	
+			axisY: {
+				title: "Billions of Barrels",
+				titleFontColor: "#4F81BC",
+				lineColor: "#4F81BC",
+				labelFontColor: "#4F81BC",
+				tickColor: "#4F81BC"
+			},
+			axisY2: {
+				title: "Millions of Barrels/day",
+				titleFontColor: "#C0504E",
+				lineColor: "#C0504E",
+				labelFontColor: "#C0504E",
+				tickColor: "#C0504E"
+			},	
+			toolTip: {
+				shared: true
+			},
+			legend: {
+				cursor:"pointer",
+				itemclick: toggleDataSeries
+			},
+			data: [{
+				type: "column",
+				name: "Proven Oil Reserves (bn)",
+				legendText: "Proven Oil Reserves",
+				showInLegend: true, 
+				dataPoints:[
+					{ label: "Saudi", y: 266.21 },
+					{ label: "Venezuela", y: 302.25 },
+					{ label: "Iran", y: 157.20 },
+					{ label: "Iraq", y: 148.77 },
+					{ label: "Kuwait", y: 101.50 },
+					{ label: "UAE", y: 97.8 }
+				]
+			},
+			{
+				type: "column",	
+				name: "Oil Production (million/day)",
+				legendText: "Oil Production",
+				axisYType: "secondary",
+				showInLegend: true,
+				dataPoints:[
+					{ label: "Saudi", y: 10.46 },
+					{ label: "Venezuela", y: 2.27 },
+					{ label: "Iran", y: 3.99 },
+					{ label: "Iraq", y: 4.45 },
+					{ label: "Kuwait", y: 2.92 },
+					{ label: "UAE", y: 3.1 }
+				]
+			}]
+		});
+		chart.render();
+	}
+
+
+
 	// END PUBLIC FUNCTIONS
 
 	//init page
 	$scope.schedaArticolo();
+	$scope.loadChart();
 	//$scope.getCategorieArticoliList();
 	
 	//private functions
 	
+	function toggleDataSeries(e) {
+		if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+			e.dataSeries.visible = false;
+		}
+		else {
+			e.dataSeries.visible = true;
+		}
+		chart.render();
+	}
+
 
 }]);
